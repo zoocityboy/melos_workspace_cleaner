6d9631cb7746aae6049d0a4c8f71896cd8949a01
zoocityboy <zoocityboy@gmail.com>
2024-01-29 11:54:19 +0100

    Update MwcConfig initialization and add support for reading patterns from melos.yaml

--- bin/mwc.dart
+++ bin/mwc.dart
@@ -3,7 +3,7 @@ import 'package:mwc/mwc.dart';
 /// A class that represents the MWC (Melos Workspace Cleaner) command.
 Future<void> main(List<String> arguments) async {
   /// The configuration used by this command.
-  final config = MwcConfig();
+  final config = MwcConfig.fromConfig();
 
   /// Runner initialization
   final cleaner = Mwc(config: config);
--- lib/src/mwc_config.dart
+++ lib/src/mwc_config.dart
@@ -1,4 +1,8 @@
+import 'dart:io';
+
 import 'package:glob/glob.dart';
+import 'package:meta/meta.dart';
+import 'package:yaml/yaml.dart';
 
 /// Represents the configuration for Melos.
 ///
@@ -11,9 +15,27 @@ class MwcConfig {
   /// The [patterns] parameter is an optional list of file patterns
   /// to be cleaned.
   /// If no patterns are provided, the default patterns will be used.
-  MwcConfig({
-    this.patterns = const ['**/pubspec_overrides.yaml', '**/pubspec.lock'],
+  MwcConfig._({
+    this.patterns = defaultPatterns,
   });
+
+  /// Creates a new instance of [MwcConfig] for testing purposes.
+  @visibleForTesting
+  factory MwcConfig.dartTest({List<String>? patterns}) {
+    return MwcConfig._(patterns: patterns ?? defaultPatterns);
+  }
+
+  /// Creates a new instance of [MwcConfig] from a `melos.yaml` .
+  factory MwcConfig.fromConfig() {
+    final patterns = MwcConfig.parseConfig();
+    return MwcConfig._(patterns: patterns ?? defaultPatterns);
+  }
+
+  /// The default patterns used by the MWCConfig class.
+  static const List<String> defaultPatterns = [
+    '**/pubspec_overrides.yaml',
+    '**/pubspec.lock',
+  ];
 
   /// The list of patterns to be cleaned.
   final List<String> patterns;
@@ -39,5 +61,22 @@ class MwcConfig {
       pattern = patterns.first;
     }
     return pattern;
+  }
+
+  /// Returns a list of patterns from a `melos.yaml` file.
+  static List<String>? parseConfig() {
+    final melosFile = File('melos.yaml');
+    final mwcFile = File('mwc.yaml');
+    return parseYamlConfig(mwcFile) ?? parseYamlConfig(melosFile);
+  }
+
+  /// Returns a list of patterns from a `mwc.yaml` file.
+  static List<String>? parseYamlConfig(File yaml) {
+    if (!yaml.existsSync()) return null;
+    final node = loadYaml(yaml.readAsStringSync())['mwc'] as YamlList?;
+    if (node != null) {
+      return List<String>.from(node.value);
+    }
+    return null;
   }
 }
