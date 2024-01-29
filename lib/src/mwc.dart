f57a6d631b8f584f1763a95887b6354b93658796
zoocityboy <zoocityboy@gmail.com>
2024-01-29 09:12:09 +0100

    chore: analyzer

--- lib/src/mwc.dart
+++ lib/src/mwc.dart
@@ -2,28 +2,25 @@ import 'dart:io';
 
 import 'package:glob/list_local_fs.dart';
 import 'package:mason_logger/mason_logger.dart';
-import 'package:meta/meta.dart';
-
-import 'mwc_config.dart';
+import 'package:mwc/src/mwc_config.dart';
 
 /// A class that represents the MWC (Melos Workspace Cleaner) command.
 class Mwc {
+  /// Creates a new instance of the MWC (Melos Workspace Cleaner) command.
+  Mwc({
+    required this.config,
+  });
+
   /// The configuration used by this command.
   final MwcConfig config;
 
   /// The logger used by this command.
   Logger logger = Logger();
 
-  /// Logger setter for testing.
-  @visibleForTesting
-  set testLogger(Logger value) => logger = value;
-  Mwc({
-    required this.config,
-  });
-
   /// Cleans the given list of [files] by deleting them.
   ///
-  /// The cleaning process is performed asynchronously, and the progress of the operation
+  /// The cleaning process is performed asynchronously,
+  /// and the progress of the operation
   /// can be tracked using the [progress] object.
   ///
   /// Throws an exception if any error occurs during the cleaning process.
@@ -31,7 +28,7 @@ class Mwc {
     for (final file in files) {
       progress.update('Cleaning [${file.path}]');
       await Future.wait([
-        Future.delayed(const Duration(milliseconds: 50)),
+        Future<void>.delayed(const Duration(milliseconds: 50)),
         File(file.path).delete(),
       ]);
     }
@@ -39,8 +36,10 @@ class Mwc {
 
   /// Runs the MWC (Melos Workspace Cleaner) command.
   ///
-  /// This method executes the MWC command, which is responsible for cleaning up the Melos workspace.
-  /// It performs various cleanup tasks such as removing temporary files, cleaning build artifacts, etc.
+  /// This method executes the MWC command, which is responsible for
+  /// cleaning up the Melos workspace.
+  /// It performs various cleanup tasks such as removing temporary files,
+  /// cleaning build artifacts, etc.
   ///
   /// Usage:
   /// ```dart
@@ -51,7 +50,8 @@ class Mwc {
   Future<void> run() async {
     final progress = logger.progress('Removing...');
     try {
-      final files = config.glob.listSync(followLinks: false).toList()..sort((a, b) => b.path.compareTo(a.path));
+      final files = config.glob.listSync(followLinks: false).toList()
+        ..sort((a, b) => b.path.compareTo(a.path));
       if (files.isEmpty) {
         progress.complete('No files to clean');
         logger.detail('pattern: ${config.formatedPatterns}');
--- lib/src/mwc_config.dart
+++ lib/src/mwc_config.dart
@@ -2,23 +2,27 @@ import 'package:glob/glob.dart';
 
 /// Represents the configuration for Melos.
 ///
-/// The [MwcConfig] class contains a list of patterns that specify the files to be cleaned.
-/// By default, the patterns include 'pubspec_overrides.yaml' and 'pubspec.lock'.
+/// The [MwcConfig] class contains a list of patterns that specify the files to
+/// be cleaned.By default, the patterns include 'pubspec_overrides.yaml'
+/// and 'pubspec.lock'.
 class MwcConfig {
-  final List<String> patterns;
-
   /// Creates a new instance of [MwcConfig].
   ///
-  /// The [patterns] parameter is an optional list of file patterns to be cleaned.
+  /// The [patterns] parameter is an optional list of file patterns
+  /// to be cleaned.
   /// If no patterns are provided, the default patterns will be used.
   MwcConfig({
     this.patterns = const ['**/pubspec_overrides.yaml', '**/pubspec.lock'],
   });
 
+  /// The list of patterns to be cleaned.
+  final List<String> patterns;
+
   /// Returns a [Glob] object based on the formatted patterns.
   ///
-  /// The formatted patterns are obtained by joining the elements of the [patterns] list
-  /// using commas and enclosing them in curly braces if there is more than one pattern.
+  /// The formatted patterns are obtained by joining the elements
+  /// of the [patterns] list using commas and enclosing them in curly braces
+  /// if there is more than one pattern.
   /// If the [patterns] list is empty, an exception is thrown.
   Glob get glob => Glob(formatedPatterns);
 
