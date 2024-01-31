35a908d3a5a2982f6b2fea62034c8a90facc0260
zoocityboy <zoocityboy@gmail.com>
2024-01-31 09:08:53 +0100

    Update CI and health workflows, add MWC library

--- lib/mwc.dart
+++ lib/mwc.dart
@@ -1,6 +1,8 @@
-/// Support for doing something awesome.
+/// MWC - Melos Workspace Cleaner
 ///
-/// More dartdocs go here.
+/// Melos Workspace Cleaner is a tool designed for cleaning monorepo workspaces built on the [Melos](https://melos.invertase.dev/~melos-latest).
+/// This tool provides an straightforward solution for maintaining and managing your monorepo project,
+/// especially when dealing with an extensive codebase or a multi-project environment.
 library;
 
 export 'src/mwc.dart'
